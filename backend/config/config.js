3c0d00b827f195827cfd0e5c49a0553351329983
lokeshspeshway <lokeshk@speshway.com>
2025-08-02 10:08:32 +0530

    Create config.js

---
+++ backend/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "your_db_password",
+    database: "mymenu_db",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
